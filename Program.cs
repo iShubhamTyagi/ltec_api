c12a9163206f961742385a51330b7e37f1e3a71a
shubhamtyagi <ishubhamtyagi2021@gmail.com>
2023-08-20 13:21:19 +0530

    CORS to include the latest URL

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("OpenCORS",
-        builder => builder.WithOrigins("https://ltcheckbharat.netlify.app", "http://localhost:3000")
+        builder => builder.WithOrigins("https://ltcheckbharat.com", "http://localhost:3000")
         .AllowAnyHeader()
         .AllowAnyMethod());
 });
