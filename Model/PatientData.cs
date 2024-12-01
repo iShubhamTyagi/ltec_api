132e35dab0801c4cb31c545264e90934cf422f58
iShubhamTyagi <84590012+iShubhamTyagi@users.noreply.github.com>
2024-12-01 09:48:05 +0530

    Fixed null ref build errors

--- Model/PatientData.cs
+++ Model/PatientData.cs
@@ -2,25 +2,25 @@
 {
     public class PatientData
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
 
-        public string Disease { get; set; }
-        public string Age { get; set; }
-        public string Id { get; set; }
-        public string Sex { get; set; }
-        public Dictionary<string, string> Answers { get; set; }
-        public Dictionary<string, string> Verdicts { get; set; }
-        public string OverallVerdict { get; set; }
-        public int Duration { get; set; }
+        public string Disease { get; set; } = string.Empty;
+        public string Age { get; set; } = string.Empty;
+        public string Id { get; set; } = string.Empty;
+        public string Sex { get; set; } = string.Empty;
+        public Dictionary<string, string> Answers { get; set; } = new Dictionary<string, string>();
+        public Dictionary<string, string> Verdicts { get; set; } = new Dictionary<string, string>();
+        public string OverallVerdict { get; set; } = string.Empty;
+        public int Duration { get; set; } = 0
 
-        public string Date { get; set; }
-        public string Time { get; set; }
+        public string Date { get; set; }  = string.Empty;
+        public string Time { get; set; }  = string.Empty;
 
-        public Dictionary<string, string> Answers1 { get; set; }
-        public Dictionary<string, string> Answers2 { get; set; }
-        public Dictionary<string, string> Answers3 { get; set; }
-        public Dictionary<string, string> Answers4 { get; set; }
+        public Dictionary<string, string> Answers1 { get; set; } = new Dictionary<string, string>();
+        public Dictionary<string, string> Answers2 { get; set; } = new Dictionary<string, string>();
+        public Dictionary<string, string> Answers3 { get; set; } = new Dictionary<string, string>();
+        public Dictionary<string, string> Answers4 { get; set; } = new Dictionary<string, string>();
     }
 
 }
